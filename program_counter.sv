1ca6ca6bf85f45b72074b135f0afee9a5b85e180
Daniel Lee <danielglee01@gmail.com>
2026-08-31 08:47:40 -0700

    finished program counter

--- program_counter.sv
+++ program_counter.sv
@@ -1,4 +1,4 @@
-module program_counter import CPU_Parameters::*; (clk, reset, PC_value);
+module program_counter import CPU_parameters::*; (clk, reset, PC_value);
 	input logic clk, reset;
 	output logic [PC_WIDTH-1:0] PC_value;
 	
@@ -7,7 +7,7 @@ module program_counter import CPU_Parameters::*; (clk, reset, PC_value);
 			PC_value <= 0;
 		end
 		else begin
-			PC_value <= PC_Value + 4;
+			PC_value <= PC_value + 4;
 		end
 	end
 endmodule
@@ -20,5 +20,23 @@ module program_counter_testbench();
 	
 	program_counter dut (.clk, .reset, .PC_value);
 	
+	// Set up a simulated clock.
+	parameter CLOCK_PERIOD = 100;
+	initial begin
+		clk <= 0;
+		forever #(CLOCK_PERIOD/2) clk <= ~clk; // Forever toggle the clock
+	end
 	
+	initial begin
+		reset <= 0; 																				     @(posedge clk);
+																											  @(posedge clk);
+		reset <= 1; 																				     @(posedge clk);
+																										     @(posedge clk);
+		reset <= 0; 																				     @(posedge clk);
+																										     @(posedge clk);
+		reset <= 1; 																				     @(posedge clk);
+																										     @(posedge clk);
+		reset <= 0; 																				     @(posedge clk); // PC counter will keep incrementing by 4 unless reset
+																										     @(posedge clk);
+	end
 endmodule
